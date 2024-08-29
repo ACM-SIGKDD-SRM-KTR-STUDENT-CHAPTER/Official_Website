6abc382b1d8675ef6bee29659e6b179fb1897704
ADITYA VERMA <adityav1304@gmail.com>
2024-08-29 22:48:42 +0530

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -19,6 +19,36 @@ function Projects() {
             </div>
           </div>
           <div className="proj-card-container">
+          <ProjectCard
+              imgSrc="https://miro.medium.com/v2/resize:fit:1400/1*tL7cirx4VR8coRg5XLwMVw.gif"
+              title="Keyword Extraction"
+              role="9 August 2024"
+              Mediumlink="https://github.com/mushir2004/Keyword-extraction"
+            />
+          <ProjectCard
+              imgSrc="https://miro.medium.com/v2/resize:fit:1400/1*tL7cirx4VR8coRg5XLwMVw.gif"
+              title="Keyword Extraction"
+              role="9 August 2024"
+              Mediumlink="https://github.com/mushir2004/Keyword-extraction"
+            />
+            <ProjectCard
+              imgSrc="https://private-user-images.githubusercontent.com/143888880/356899035-3afd50ad-4311-4882-a75d-1acd600c58cd.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjQ5NTEwMDMsIm5iZiI6MTcyNDk1MDcwMywicGF0aCI6Ii8xNDM4ODg4ODAvMzU2ODk5MDM1LTNhZmQ1MGFkLTQzMTEtNDg4Mi1hNzVkLTFhY2Q2MDBjNThjZC5qcGc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwODI5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDgyOVQxNjU4MjNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0xMDIwZTk1M2I5ZmNhYzRmYWFhYWQxZWNiMTZlZjE2YTE1MDc1ZmVlMmZhODZlZDM5MWZjY2VjN2MzNTk4MjVlJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.iZnqBgNDJ3Vzjw6l6ofN1fdjQR86vOv1pHPALXP0pgU"
+              title="ACM FARM HEALTH'24"
+              role="12 August 2024"
+              Mediumlink="https://github.com/Srijansarkar17/ACM_FarmHealth"
+            />
+          <ProjectCard
+              imgSrc="https://private-user-images.githubusercontent.com/143888880/349614867-81c8cb8c-97a9-4ecd-97ea-3ecfef516d42.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjQ5NTA2MzgsIm5iZiI6MTcyNDk1MDMzOCwicGF0aCI6Ii8xNDM4ODg4ODAvMzQ5NjE0ODY3LTgxYzhjYjhjLTk3YTktNGVjZC05N2VhLTNlY2ZlZjUxNmQ0Mi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwODI5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDgyOVQxNjUyMThaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iNmNmMzFhMjBkY2Q2ZjRhOTdhMzliNDEzZDRhOGEwODM2ZGE4Nzg5ZTQ2MjFlNzA1MjIwZjhjYmYzY2UwYmNlJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.Iaqhdi7zq8Z56HiQw1Iym6S3Z_zo2-AA3pO5lOlFDmc"
+              title="Automatic Colorization of Photos using Deep Neural Networks"
+              role="18 July 2024"
+              Mediumlink="https://github.com/Adityapratapsingh28/Automatic-Colorization-of-Photos-using-Deep-Neural-Networks"
+            />
+            <ProjectCard
+              imgSrc="https://www.citlprojects.com/hs-fs/hubfs/blogs/deblurring-blurred-image-gan-cnn.jpg?width=616&height=412&name=deblurring-blurred-image-gan-cnn.jpg"
+              title="Image Deblurring using GAN"
+              role="16 July 2024"
+              Mediumlink="https://github.com/armaanranjan/IMAGE-DEBLURRING-USING-GAN"
+            />
             <ProjectCard
               imgSrc="https://s3.amazonaws.com/aurochssolutions.com-images/sales_forecasting_01_10ee95f93b.jpeg"
               title="Sales Forecasting"
