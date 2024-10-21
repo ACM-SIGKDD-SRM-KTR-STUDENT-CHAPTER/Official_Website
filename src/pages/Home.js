bd4967b5e459befba1d75f7427ed96462e98c15c
ADITYA VERMA <adityav1304@gmail.com>
2024-10-21 14:19:39 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ function Home() {
     className="infoo"
         chaptername="SRM IST KTR"
         description="Dive into the exciting domains of Data Science, Machine Learning, Deep Learning, and Natural Language Processing with us. Join our community of passionate learners and innovators as we explore the cutting-edge technologies shaping the future"
-        link="/team"
+        link="https://hackofduty.vercel.app/"
       />
       <Quotes/>
     {/* <UpcomingEvent /> */}
